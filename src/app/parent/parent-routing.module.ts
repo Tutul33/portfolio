dafbef301c564cc757989ae0fbcd5a89f1404f6d
Tutul33 <tutulcou@gmail.com>
2024-10-07 15:08:53 +0600

    dx

--- src/app/parent/parent-routing.module.ts
+++ src/app/parent/parent-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-   { path: 'home', component: HomeComponent, data: { breadcrumb: 'Home' } },
-//   { path: 'signup', component: SignupComponent, data: { breadcrumb: '' } },
-//   { path: 'forgotpassword', component: ForgotpasswordComponent, data: { breadcrumb: '' } },
-//   { path: 'changePassword', component: ChangePasswordComponent, data: { breadcrumb: '' } },
+  { path: 'home', component: HomeComponent, data: { breadcrumb: 'Home' } },
 ];
 
 @NgModule({
