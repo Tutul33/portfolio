0e9314721e0aa6a06126bd84ca517eb180e8eafc
Tutul Chakma <tutulcou@gmail.com>
2024-10-13 23:30:08 +0600

    s

--- src/app/parent/skills/skills.component.ts
+++ src/app/parent/skills/skills.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.scss']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit, AfterViewInit {
 
+  ngOnInit() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+    });
+  }
+
+  ngAfterViewInit() {
+    const progressBars = document.querySelectorAll('.progress-bar');
+    progressBars.forEach((bar:any) => {
+      const value = bar.getAttribute('aria-valuenow');
+      if (value) {
+        bar.style.width = value + '%';
+      }
+    });
+  }
 }
